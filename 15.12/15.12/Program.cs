8e99d2d72ce3539716819bf0e58897825815e8aa
Waleryszak <147153732+Waleryszak@users.noreply.github.com>
2023-12-15 13:48:15 +0100

    Add files via upload

---
+++ 15.12/15.12/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _15._12
+{
+    class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+
+        public Book(string title, string author, int year, int price) 
+        { 
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+            this.Price = price;
+        }
+        public int CompareTo(Book? other)
+        {
+            if (other == null) return 1;
+            return Price.CompareTo(other.Price);
+        }
+        public override string ToString()
+        {
+            return $"{this.Title},{this.Author},{this.Year}, cenus:{this.Price}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            Console.WriteLine(" ksiazki:");
+            books.Add(new Book("Mały ksiazke","Szymon",1999,1112));
+            books.Add(new Book("aaaa", "Władek", 1789, 20));
+            books.Add(new Book("bbbbb", "Jan", 2001, 30));
+            books.Add(new Book("cccc", "Dominik", 1670, 40));
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\n");
+            books.Sort();
+            Console.WriteLine("Posortowane ksiazki:\n");
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+
+            Console.WriteLine("\n sortowanie rok");
+            var sortedByYear =  books.OrderBy(x => x.Year);
+            foreach (Book book in sortedByYear)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\n sortowanie tytul");
+            var sortedBytitle = books.OrderByDescending(x => x.Title);
+            foreach (Book book in sortedBytitle)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\n sortowanie ceny nie rosnaco a nastepnie według roku od najstarszej ksiazki");
+            var sortedByPriceThenYear = books.OrderByDescending(x => x.Price).ThenBy(x =>x.Year);
+            foreach (Book book in sortedByPriceThenYear)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
---
+++ interfejsy2/interfejsy2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Globalization;
+
+namespace interfejsy2
+{
+    class EngineFailureException : Exception
+    {
+        public EngineFailureException(string message) : base(message)
+        {
+
+        }
+    }
+
+    interface IMovable
+    {
+        void Drive(int distance);
+        event EventHandler<Car> Broken;
+    }
+
+    class Car : IMovable
+    {
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+        public int Mileage { get; set; }
+
+        public Car(string brand, string model, string color, int capacity)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;  
+            Capacity = capacity;
+            Broken += (sender, args) => Console.WriteLine("Samochód rozjebał sie na amen");
+        }
+
+        public void Drive(int distance)
+        {
+            Mileage += distance;
+            Random rand = new Random();
+            int num = rand.Next(1, 100);
+            if (num < 10) 
+            {
+                throw new EngineFailureException("Silnik kaput sznela niśt worken!");            
+            }
+        }
+
+        public event EventHandler<Car> Broken;
+
+        public void OnBroken()
+        {
+            if (Broken != null) 
+            {
+                Broken(this, this);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Brand},{Model},{Capacity},{Mileage}";
+        }
+    }
+
+    class Mechanic
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public Mechanic(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public delegate bool Repair(Car c);
+
+        public void PerformRepair(Car c, Repair r )
+        {
+            bool result = r(c);
+            Console.WriteLine($"{Name}, {Surname} Naprawiał {c.Brand} {c.Model} {(result ? "naprawiony" : "nie naprawiony")}");
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
